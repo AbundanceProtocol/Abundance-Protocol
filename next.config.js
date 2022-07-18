cda8536827dd9dbe106abe4d8e98c44f3c154dff
MikeNatanzon <michael@buildingweb4.io>
2022-07-18 00:35:34 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,  
   env: {
     ENVIRONMENT: process.env.ENVIRONMENT
+  },
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    emotion: true
   }
 }
 
