01e75cd7a0267a175d192eb23e06a652dc8a1c83
Mike Natanzon <michael@buildingweb4.io>
2023-04-23 01:17:34 -0400

    Revised menu to align with project whitepaper vision

--- pages/assets/button.js
+++ pages/assets/button.js
@@ -1,10 +1,10 @@
-import { FaSearch, FaCode, FaUser, FaLightbulb, FaKey, FaLockOpen, FaGlobe, FaPen, FaCoins, FaLink, FaAddressCard, FaWallet, FaAward, FaQuestionCircle, FaMap, FaCogs, FaFileAlt, FaGithub, FaMediumM, FaYoutube, FaTwitter, FaAt, FaDiscord, FaFolderOpen, FaTasks, FaScroll, FaSearchDollar } from 'react-icons/fa';
+import { FaSearch, FaCode, FaUser, FaLightbulb, FaKey, FaLockOpen, FaGlobe, FaPen, FaCoins, FaLink, FaAddressCard, FaWallet, FaAward, FaQuestionCircle, FaMap, FaCogs, FaFileAlt, FaGithub, FaMediumM, FaYoutube, FaTwitter, FaAt, FaDiscord, FaFolderOpen, FaTasks, FaScroll, FaSearchDollar, FaHandshake, FaRegListAlt, FaToolbox } from 'react-icons/fa';
 import OpenSeaIcon from './OpenSeaIcon';
 import { GiTwoCoins, GiReceiveMoney } from 'react-icons/gi'
-import { BsBarChartFill, BsFillDiagram3Fill } from 'react-icons/bs'
-import { MdCastConnected, MdNotifications, MdFavorite, MdFilterFrames, MdMail, MdWork } from 'react-icons/md'
-import { RiTeamFill } from 'react-icons/ri'
-import { HiBadgeCheck } from 'react-icons/hi'
+import { BsBarChartFill, BsFillDiagram3Fill, BsFillHandThumbsDownFill, BsListColumnsReverse } from 'react-icons/bs'
+import { MdCastConnected, MdNotifications, MdFavorite, MdFilterFrames, MdMail, MdWork, MdRequestPage } from 'react-icons/md'
+import { RiTeamFill, RiFileSearchFill } from 'react-icons/ri'
+import { HiBadgeCheck, HiThumbDown, HiUserGroup, HiViewGridAdd } from 'react-icons/hi'
 import { IoReader } from 'react-icons/io5'
 import { SiSubstack } from 'react-icons/si'
 
@@ -15,42 +15,107 @@ const Buttons = () => {
 export default Buttons;
 
 const button = {
-    'top-menu': ['portal', 'studio', 'fund', 'ecosystem', 'connect'],
+    'top-menu': ['portal', 'studio', 'consensus', 'funding', 'ecosystem'],
     'nav-menu': {
-        'Home': ['Vision', 'Roadmap', 'Mechanism', 'Whitepaper', 'FAQ', 'Blog'],
-        'Portal': ['Profile', 'Wallet', 'Expertise', 'Portfolio', 'Subscriptions', 'Inbox'],
+        'Home': ['Vision', 'Roadmap', 'Mechanism', 'Whitepaper', 'FAQ', 'Blog', 'Contact'],
+        'Portal': ['Profile', 'Wallet', 'Expertise', 'Portfolio'],
+        'Studio': ['Create', 'Contribute', 'Collaborate'],
+        'Consensus': ['Propose', 'Validate', 'Challenge', 'Explore', 'Notifications'],
         'Fund': ['Project Funding', 'Review Funding', 'Funding Proposal', 'Search Requests'],
-        'Create': ['Project', 'Post', 'Initial Review', 'Collaboration', 'Reviews'],
-        'Search': ['Posts', 'Users', 'Proposals', 'Categories'],
-        'Connect': ['GitHub', 'Discord', 'OpenSea', 'YouTube', 'Twitter', 'Email'],
-        'Explore': ['Search', 'Trending', 'Curated', 'Favorites', 'Feed'],
+        'Ecosystem': ['Communities', 'Resources', 'Search', 'Trending', 'Curated', 'Favorites', 'Feed'],
+        // 'Connect': ['GitHub', 'Discord', 'OpenSea', 'YouTube', 'Twitter', 'Email'],
     },
-    'GitHub': {
-        link: false,
-        url: 'https://github.com/buildingweb4/abundance-protocol',
-        menu: 'Connect',
-        description: 'Help us develop the protocol on GitHub',
+
+
+
+
+    ////// MENUS //////
+
+    'portal': {
+        link: '/portal',
+        menu: 'Portal',
+        description: 'Your activity hub',
+        account: true,
+        working: true,
+        icon: FaUser
+    },
+    'studio': {
+        link: '/create',
+        menu: 'Studio',
+        description: 'Create. Contribute. Collaborate',
+        account: true,
+        working: true,
+        icon: FaPen
+    },
+    'consensus': {
+        link: '/consensus',
+        menu: 'Consensus',
+        description: 'Proof-of-Impact Consensus Validation',
+        account: true,
+        working: true,
+        icon: FaHandshake
+    },
+    'funding': {
+        link: '/fund',
+        menu: 'Fund',
+        description: 'Give and get funding',
+        account: true,
+        working: true,
+        icon: FaCoins
+    },
+    'ecosystem': {
+        link: '/explore',
+        menu: 'Ecosystem',
+        description: 'Explore the ecosystem, your communities, and resources',
         account: false,
         working: true,
-        icon: FaGithub
+        icon: FaGlobe
     },
-    'Discord': {
-        link: false,
-        url: 'https://discord.com/invite/sHcV7g3nqu',
-        menu: 'Connect',
-        description: 'Join our Discord community',
+
+
+
+
+    ////// HOME //////
+
+    'Vision': {
+        link: '/',
+        menu: 'Home',
+        description: 'The Abundance Protocol vision for a decentralized economy',
         account: false,
         working: true,
-        icon: FaDiscord
+        icon: FaLightbulb
     },
-    'OpenSea': {
-        link: false,
-        url: 'https://opensea.io/web4',
-        menu: 'Connect',
-        description: "Get our project's NFTs",
+    'Roadmap': {
+        link: '/roadmap',
+        menu: 'Home',
+        description: "The Protocol's Master Plan",
+        account: false,
+        working: false,
+        icon: FaMap
+    },
+    'Mechanism': {
+        link: '/mechanism',
+        menu: 'Home',
+        description: 'How it all works',
+        account: false,
+        working: false,
+        icon: FaCogs
+    },
+    'Whitepaper': {
+        link: '/white-paper',
+        menu: 'Home',
+        description: 'In-depth explanation of the Protocol',
         account: false,
         working: true,
-        icon: OpenSeaIcon
+        icon: FaFileAlt
+    },
+    'FAQ': {
+        link: '/faq',
+        menu: 'Home',
+        description: 'Frequently asked questions',
+        account: false,
+        working: false,
+        icon: FaQuestionCircle
     },
     'Blog': {
         link: false,
@@ -61,144 +126,141 @@ const button = {
         working: true,
         icon: SiSubstack
     },
-    'YouTube': {
-        link: false,
-        url: 'https://www.youtube.com/@AbundanceProtocol/',
-        menu: 'Connect',
-        description: 'Subscribe to our channel',
+    'Contact': {
+        link: '/contact',
+        menu: 'Home',
+        description: 'Connect with us',
         account: false,
-        working: true,
-        icon: FaYoutube
-    },
-    'Twitter': {
-        link: false,
-        url: 'https://twitter.com/BuildingWeb4',
-        menu: 'Connect',
-        description: 'Follow us on Twitter',
-        account: false,
-        working: true,
-        icon: FaTwitter
-    },
-    'Email': {
-        link: false,
-        url: 'mailto:info@buildingweb4.io',
-        menu: 'Connect',
-        description: 'Contact us',
-        account: false,
-        working: true,
-        icon: FaAt
-    },
-
-    'studio': {
-        link: '/create',
-        menu: 'Create',
-        account: true,
-        working: true,
-        icon: FaPen
-    },
-    'fund': {
-        link: '/fund',
-        menu: 'Fund',
-        account: true,
-        working: true,
-        icon: FaCoins
-    },
-    'connect': {
-        link: '/connect',
-        menu: 'Connect',
-        account: false,
-        working: true,
+        working: false,
         icon: FaLink
     },
-    'ecosystem': {
-        link: '/explore',
-        menu: 'Explore',
-        account: false,
-        working: true,
-        icon: FaGlobe
-    },
-    'portal': {
-        link: '/portal',
+
+    
+
+    ////// PORTAL //////
+
+    'Profile': {
+        link: '/profile',
         menu: 'Portal',
-        description: 'review categories',
+        description: 'Update your PFP and bio',
         account: true,
         working: true,
-        icon: FaUser
+        icon: FaAddressCard
     },
-    'search': {
-        link: '/search',
-        menu: 'Search',
-        description: 'review categories',
-        account: false,
+    'Wallet': {
+        link: '/wallet',
+        menu: 'Portal',
+        description: 'Manage your funds, investments & bids',
+        account: true,
+        working: true,
+        icon: FaWallet
+    },
+    'Expertise': {
+        link: '/expertise',
+        menu: 'Portal',
+        description: 'Review your scores across fields',
+        account: true,
         working: false,
-        icon: FaSearch
+        icon: FaAward
+    },
+    'Portfolio': {
+        link: '/portfolio',
+        menu: 'Portal',
+        description: 'Check your projects & proposals',
+        account: true,
+        working: true,
+        icon: FaFolderOpen
     },
 
-    'Search': {
-        link: '/searchbar',
-        menu: 'Explore',
-        description: 'Search for posts, users and proposals',
-        account: false,
-        working: false,
-        icon: FaSearch
-    },
-    'Trending': {
-        link: '/trending',
-        menu: 'Explore',
-        description: 'Trending projects and posts',
-        account: false,
-        working: true,
-        icon: BsBarChartFill
-    },
-    'Curated': {
-        link: '/curated',
-        menu: 'Explore',
-        description: 'Curated project and posts',
-        account: false,
-        working: false,
-        icon: MdFilterFrames
-    },
-    'Favorites': {
-        link: '/favorites',
-        menu: 'Explore',
-        description: 'Your favorite project categories',
+    
+
+
+    ////// STUDIO //////
+
+    'Create': {
+        link: '/create-post',
+        menu: 'Studio',
+        description: 'Create a project',
         account: true,
         working: false,
-        icon: MdFavorite
+        icon: MdWork
     },
-    'Feed': {
-        link: '/feed',
-        menu: 'Explore',
-        description: 'Latest projects and posts',
+    'Contribute': {
+        link: '/contribute',
+        menu: 'Studio',
+        description: 'Contribute to an existing project',
         account: true,
-        working: true,
-        icon: FaScroll
-    },
-    'Vision': {
-        link: '/',
-        menu: 'Home',
-        description: 'The Abundance Protocol vision for a decentralized economy',
-        account: false,
-        working: true,
-        icon: FaLightbulb
-    },
-    'FAQ': {
-        link: '/faq',
-        menu: 'Home',
-        description: 'Frequently asked questions',
-        account: false,
         working: false,
-        icon: FaQuestionCircle
+        icon: HiViewGridAdd
     },
-    'Mechanism': {
-        link: '/mechanism',
-        menu: 'Home',
-        description: 'How it all works',
-        account: false,
+    'Collaborate': {
+        link: '/collaboration',
+        menu: 'Studio',
+        description: 'Collaborate with others on a project',
+        account: true,
         working: false,
-        icon: FaCogs
+        icon: RiTeamFill
     },
 
+    
+
+
+    ////// CONSENSUS //////
+
+    'Propose': {
+        link: '/init-review',
+        menu: 'Consensus',
+        description: 'Create a proposal',
+        account: true,
+        working: false,
+        icon: MdRequestPage
+    },
+    'Validate': {
+        link: '/validation-pool',
+        menu: 'Consensus',
+        description: 'Join a validation pool',
+        account: true,
+        working: false,
+        icon: FaRegListAlt
+    },
+    'Challenge': {
+        link: '/challenge',
+        menu: 'Consensus',
+        description: 'Challenge a proposal',
+        account: true,
+        working: false,
+        icon: BsFillHandThumbsDownFill
+    },
+    'Explore': {
+        link: '/explore-proposals',
+        menu: 'Consensus',
+        description: 'Explore proposals',
+        account: false,
+        working: false,
+        icon: RiFileSearchFill
+    },
+    'Notifications': {
+        link: '/inbox',
+        menu: 'Consensus',
+        description: 'Validation notifications',
+        account: true,
+        working: false,
+        icon: MdMail
+    },
+
+    
+
+
+    // FUNDING
+
+    'Project Funding': {
+        link: '/project-funding',
+        menu: 'Fund',
+        description: 'Request funding for a public goods project',
+        account: true,
+        working: false,
+        icon: GiTwoCoins
+    },
     'Review Funding': {
         link: '/funding',
         menu: 'Fund',
@@ -215,153 +277,136 @@ const button = {
         working: false,
         icon: FaGlobe
     },
-
-    'Profile': {
-        link: '/profile',
-        menu: 'Portal',
-        description: 'Update your PFP and bio',
-        account: true,
-        working: true,
-        icon: FaAddressCard
-    },
-    'Wallet': {
-        link: '/wallet',
-        menu: 'Portal',
-        description: 'Manage your funds, investments, proposals & bids',
-        account: true,
-        working: true,
-        icon: FaWallet
-    },
-    'Expertise': {
-        link: '/expertise',
-        menu: 'Portal',
-        description: 'Review your scores across fields',
-        account: true,
-        working: false,
-        icon: FaAward
-    },
-    'Portfolio': {
-        link: '/portfolio',
-        menu: 'Portal',
-        description: 'Check your projects, posts and reviews',
-        account: true,
-        working: true,
-        icon: FaFolderOpen
-    },
-    'Subscriptions': {
-        link: '/subscriptions',
-        menu: 'Portal',
-        description: 'Subscribe to Categories to participate in reviews',
-        account: true,
-        working: false,
-        icon: MdCastConnected
-    },
-    'Inbox': {
-        link: '/inbox',
-        menu: 'Portal',
-        description: 'Pending reviews',
-        account: true,
-        working: false,
-        icon: MdMail
-    },
-
-    'Project Funding': {
-        link: '/project-funding',
-        menu: 'Fund',
-        description: 'Request funding for a public goods project',
-        account: true,
-        working: false,
-        icon: GiTwoCoins
-    },
-    'Roadmap': {
-        link: '/roadmap',
-        menu: 'Home',
-        description: "The Protocol's Master Plan",
-        account: false,
-        working: false,
-        icon: FaMap
-    },
-    'Reviews': {
-        link: '/reviews',
-        menu: 'Create',
-        description: 'Pending reviews',
-        account: true,
-        working: false,
-        icon: FaTasks
-    },
-    'Whitepaper': {
-        link: '/white-paper',
-        menu: 'Home',
-        description: 'In-depth explanation of the Protocol',
-        account: false,
-        working: true,
-        icon: FaFileAlt
-    },
-    'Project': {
-        link: '/create-project',
-        menu: 'Create',
-        description: 'Create a project',
-        account: true,
-        working: false,
-        icon: MdWork
-    },
-    'Post': {
-        link: '/create-post',
-        menu: 'Create',
-        description: 'Create a post',
-        account: true,
-        working: true,
-        icon: FaPen
-    },
-    'Collaboration': {
-        link: '/collaboration',
-        menu: 'Create',
-        description: 'Create a collaboration',
-        account: true,
-        working: false,
-        icon: RiTeamFill
-    },
-    'Initial Review': {
-        link: '/init-review',
-        menu: 'Create',
-        description: 'Create an initial review',
-        account: true,
-        working: false,
-        icon: HiBadgeCheck
-    },
-
-    'Posts': {
-        link: '/posts',
-        menu: 'Search',
-        description: 'review categories',
-        account: false,
-        working: false,
-        icon: FaPen
-    },
-    'Users': {
-        link: '/users',
-        menu: 'Search',
-        description: 'review categories',
-        account: false,
-        working: false,
-        icon: FaPen
-    },
     'Search Requests': {
         link: '/search-requests',
-        menu: 'Explore',
+        menu: 'Fund',
         description: 'Find funding requests and auctions',
         account: false,
         working: true,
         icon: FaSearchDollar
     },
-    'Categories': {
-        link: '/categories',
-        menu: 'Search',
-        description: 'review categories',
+
+    
+
+
+    ////// ECOSYSTEM //////
+    
+    'Communities': {
+        link: '/communities',
+        menu: 'Ecosystem',
+        description: 'Explore your communities',
+        account: true,
+        working: false,
+        icon: HiUserGroup
+    },
+    'Resources': {
+        link: '/resources',
+        menu: 'Ecosystem',
+        description: 'Access resources from across the ecosystem',
         account: false,
         working: false,
-        icon: BsFillDiagram3Fill
+        icon: FaToolbox
     },
+    'Trending': {
+        link: '/trending',
+        menu: 'Ecosystem',
+        description: 'Trending projects',
+        account: false,
+        working: true,
+        icon: BsBarChartFill
+    },
+    'Curated': {
+        link: '/curated',
+        menu: 'Ecosystem',
+        description: 'Curated projects',
+        account: false,
+        working: false,
+        icon: MdFilterFrames
+    },
+    'Favorites': {
+        link: '/favorites',
+        menu: 'Ecosystem',
+        description: 'Your favorite project categories',
+        account: true,
+        working: false,
+        icon: MdFavorite
+    },
+    'Search': {
+        link: '/searchbar',
+        menu: 'Ecosystem',
+        description: 'Search for communities, projects, users, and proposals',
+        account: false,
+        working: false,
+        icon: FaSearch
+    },
+    'Feed': {
+        link: '/feed',
+        menu: 'Ecosystem',
+        description: 'Latest projects',
+        account: true,
+        working: true,
+        icon: FaScroll
+    },
+
+    
+
+
+    // LINKS
+
+    // 'GitHub': {
+    //     link: false,
+    //     url: 'https://github.com/buildingweb4/abundance-protocol',
+    //     menu: 'Connect',
+    //     description: 'Help us develop the protocol on GitHub',
+    //     account: false,
+    //     working: true,
+    //     icon: FaGithub
+    // },
+    // 'Discord': {
+    //     link: false,
+    //     url: 'https://discord.com/invite/sHcV7g3nqu',
+    //     menu: 'Connect',
+    //     description: 'Join our Discord community',
+    //     account: false,
+    //     working: true,
+    //     icon: FaDiscord
+    // },
+    // 'OpenSea': {
+    //     link: false,
+    //     url: 'https://opensea.io/web4',
+    //     menu: 'Connect',
+    //     description: "Get our project's NFTs",
+    //     account: false,
+    //     working: true,
+    //     icon: OpenSeaIcon
+    // },
+    // 'YouTube': {
+    //     link: false,
+    //     url: 'https://www.youtube.com/@AbundanceProtocol/',
+    //     menu: 'Connect',
+    //     description: 'Subscribe to our channel',
+    //     account: false,
+    //     working: true,
+    //     icon: FaYoutube
+    // },
+    // 'Twitter': {
+    //     link: false,
+    //     url: 'https://twitter.com/BuildingWeb4',
+    //     menu: 'Connect',
+    //     description: 'Follow us on Twitter',
+    //     account: false,
+    //     working: true,
+    //     icon: FaTwitter
+    // },
+    // 'Email': {
+    //     link: false,
+    //     url: 'mailto:info@buildingweb4.io',
+    //     menu: 'Connect',
+    //     description: 'Contact us',
+    //     account: false,
+    //     working: true,
+    //     icon: FaAt
+    // },
   }
 
 export { button }
